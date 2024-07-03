62d1fbfde31b0b9f88a2111ad1a193d7e83a4a4c
Luis <tony_articuno@hotmail.com>
2024-07-03 18:03:16 +0200

    Add project files.

---
+++ Platforms/Android/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+using static AndroidX.Core.SplashScreen.SplashScreen;
+
+namespace AnimatedSplashScreenApp
+{
+    [Activity(Theme = "@style/Theme.Animated", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            var splash = InstallSplashScreen(this);
+            base.OnCreate(savedInstanceState);
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AnimatedSplashScreenApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
